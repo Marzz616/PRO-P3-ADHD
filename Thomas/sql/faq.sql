d3c6a7da18bcb96a5e2294e45d7e9f6281a43936
EAgivemoney <thomasmeijer07@t-mobilethuis.nl>
2024-02-29 10:43:29 +0100

    Verder gewerkt aan FAQ Database

---
+++ Thomas/sql/faq.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 28 feb 2024 om 09:43
+-- Serverversie: 8.0.31
+-- PHP-versie: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `faq`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `qa_table`
+--
+DROP DATABASE IF EXISTS 'faq';
+CREATE DATABASE IF NOT EXISTS 'faq';
+USE 'faq';
+DROP TABLE IF EXISTS `qa_table`;
+CREATE TABLE IF NOT EXISTS `qa_table` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `question` mediumtext NOT NULL,
+  `answer` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
