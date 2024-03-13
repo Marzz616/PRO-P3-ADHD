485daa3cd4b3da89ccfc296c13929a888842290f
NimrodLoozar <nimrod.lobozarl.com>
2024-03-13 19:15:21 +0100

    ADHD test erbij gedaan en faq ook

---
+++ Main/main_page/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+// Naam van de server
+$dbHost = 'localhost';
+
+// Naam van de database
+$dbName = 'project_p3';
+
+// Naam van de gebruiker
+$dbUser = 'yourideron';
+
+// Wachtwoord van de gebruiker
+$dbPass = 'Empire12!';
+
