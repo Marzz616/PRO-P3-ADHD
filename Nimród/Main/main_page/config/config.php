b0552559f53e853ead662c970bf502f25bffe4cb
NimrodLoozar <nimrod.lobozar@gmail.com>
2024-03-22 10:37:37 +0100

    Main gecopyd en geplakt in mijn map

---
+++ Nimród/Main/main_page/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+
+// Naam van de server
+$dbHost = 'localhost';
+
+// Naam van de database
+$dbName = 'project_p3';
+
+// Naam van de gebruiker
+$dbUser = "rra2309a";
+
+// Wachtwoord van de gebruiker
+$dbPass = "gOC0oTGAyEBCPIRy";
