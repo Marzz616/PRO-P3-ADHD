203ab16d11a18c304a88c9b45d7989c83ee5b62f
yup50 <147809042+yup50@users.noreply.github.com>
2024-03-30 22:24:04 +0100

    De admin page werkt als je de juiste database hebt

---
+++ Main/contactChildren/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+
+// Naam van de server
+$dbHost = 'localhost';
+
+// Naam van de database
+$dbName = 'project_p3';
+
+// Naam van de gebruiker
+$dbUser = 'yourideron';
+
+// Wachtwoord van de gebruiker
+$dbPass = 'Empire12!';
